cf3253821e229eb5d4edbac2659096c511efe7cd
Luke Benstead <kazade@gmail.com>
2011-10-15 08:16:45 +0100

    Remove kazbase to replace it with a submodule

--- src/frontend/main_window.cpp
+++ src/frontend/main_window.cpp
@@ -1,12 +1,19 @@
 #include "main_window.h"
 
 MainWindow::MainWindow():
-    window_(NULL) {
+    window_(nullptr),
+    source_view_(nullptr) {
 
     window_ = new Gtk::Window();
-    
+    source_view_ = Gtk::manage(new Gtk::Sourceview());
+    window_->add(*source_view_);
+        
     window_->signal_delete_event().connect(sigc::mem_fun(this, &MainWindow::on_delete_event));
     window_->show_all();
+}
+
+MainWindow::~MainWindow() {
+    delete window_;
 }
 
 void MainWindow::on_quit() {
--- src/frontend/main_window.h
+++ src/frontend/main_window.h
@@ -6,9 +6,11 @@
 class MainWindow {
 private:
     Gtk::Window* window_;
+    Gtk::Sourceview* source_view_; 
     
 public:
     MainWindow();
+    ~MainWindow();
     
     void on_quit();
     bool on_delete_event(GdkEventAny* event); 
